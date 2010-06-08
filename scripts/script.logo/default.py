f68f6bccc2b471f2af71c3e7804764b446207e6b
mcborzu <borzu_2000@yahoo.com>
2010-06-07 23:07:25 -0400

    MESSAGE

--- scripts/script.logo/default.py
+++ scripts/script.logo/default.py
@@ -4,8 +4,8 @@ __url__          = "http://code.google.com/p/passion-xbmc/"
 __svn_url__      = ""
 __credits__      = "Team XBMC PASSION, http://passion-xbmc.org/"
 __platform__     = "xbmc media center, [LINUX, OS X, WIN32, XBOX]"
-__date__         = "04-06-2010"
-__version__      = "1.2.1"
+__date__         = "05-06-2010"
+__version__      = "1.2.2"
 __svn_revision__  = "$Revision: 697 $"
 __XBMC_Revision__ = "20000" #XBMC Babylon
 __useragent__ = "Mozilla/5.0 (Windows; U; Windows NT 5.1; fr; rv:1.9.0.1) Gecko/2008070208 Firefox/3.6"
@@ -104,7 +104,8 @@ def get_tvid_path( tvpath ):
             return raw[0] , raw[1]
     except: 
         print_exc()
-        return ( False , False )
+        
+        return  "False" , "False" 
     
     
 def get_first_logo( tvid ):
@@ -139,12 +140,13 @@ def download_logo( url_logo , path , tvcount , name ):
             strProgressBar = str( percent )
             #print percent  #DEBUG
             DIALOG_PROGRESS.update( tvcount , "Searching: %s " %  translate_string( name ) , "Downloading: %s" % percent )
-        if os.path.exists(path):
+        if os.path.exists(path.encode("utf-8")):
             fp , h = urllib.urlretrieve( url_logo , destination , _report_hook )
             #print h
             return True
-    except :
-        print_exc()   
+    except :        
+        print "### Logo download Failed !!! (download_logo)"
+        print_exc()  
         return False
         
         
@@ -165,45 +167,58 @@ if ( __name__ == "__main__" ):
         logo_path = sys.argv[ 1 ]
         print "### path:%s###" % logo_path
         print "### Getting id"
-        tvid , logo_path = get_tvid_path( sys.argv[ 1 ] )
+        try: tvid , logo_path = get_tvid_path( sys.argv[ 1 ] )
+        except: 
+            tvid = False
+            print_exc()
         print "### id Found: %s" % tvid
-        if tvid[0:2] == "tt" : 
-            print "### IMDB id found (%s), checking for nfo" % logo_path
-            try: tvid = get_nfo_id( logo_path )
-            except:                
-                print "### Error checking for nfo: %stvshow.nfo" % logo_path.replace("\\\\" , "\\").encode("utf-8")
-                print_exc()
-        print "### get logo list for id:%s ###" % tvid
-        logo_list = get_logo_list( tvid )
-        if logo_list: 
-            print "### %s" % logo_list
-            DIALOG_PROGRESS.close
-            select = xbmcgui.Dialog().select("choose logo to download" , logo_list)
-            if select == -1: pass
-            else:
-                DIALOG_PROGRESS.create( "Logo Downloader in action ..." , "Downloading ..." )
-                
-                url_logo = BASE_URL + logo_list[select]
-                
-                if logo_path: 
-                    full_logo_path = os.path.join( logo_path , "logo.png").replace("\\\\" , "\\").encode("utf-8")
-                    print "### download logo: %s" % url_logo
-                    try:
-                        def _report_hook( count, blocksize, totalsize ):
-                            percent = int( float( count * blocksize * 100 ) / totalsize )
-                            strProgressBar = str( percent )
-                            #print percent  #DEBUG
-                            DIALOG_PROGRESS.update( percent , "downloading: %s " %  logo_list[select] , "path: %s" % full_logo_path )
-                        if os.path.exists(logo_path):
-                            fp , h = urllib.urlretrieve( url_logo , full_logo_path , _report_hook )
-                            #print h
-                            DIALOG_PROGRESS.close
-                            xbmcgui.Dialog().ok("Success" , "Logo downloaded successfully !" )
-                    except :
-                        print_exc()
-                        DIALOG_PROGRESS.close   
-                        xbmcgui.Dialog().ok("Error" , "Error downloading logo !" )
-                else: print "### Path not found"
+        if tvid:
+            if tvid[0:2] == "tt" : 
+                print "### IMDB id found (%s), checking for nfo" % logo_path
+                try: tvid = get_nfo_id( logo_path )
+                except:                
+                    print "### Error checking for nfo: %stvshow.nfo" % logo_path.replace("\\\\" , "\\").encode("utf-8")
+                    print_exc()
+            print "### get logo list for id:%s ###" % tvid
+            logo_list = get_logo_list( tvid )
+            if logo_list: 
+                print "### %s" % logo_list
+                DIALOG_PROGRESS.close
+                select = xbmcgui.Dialog().select("choose logo to download" , logo_list)
+                if select == -1: pass
+                else:
+                    DIALOG_PROGRESS.create( "Logo Downloader in action ..." , "Downloading ..." )
+                    
+                    url_logo = BASE_URL + logo_list[select]
+                    
+                    if logo_path: 
+                        full_logo_path = os.path.join( logo_path , "logo.png").replace("\\\\" , "\\").encode("utf-8")
+                        print "### download logo: %s" % url_logo
+                        print "### destination logo: %s" % full_logo_path
+                        try:
+                            def _report_hook( count, blocksize, totalsize ):
+                                percent = int( float( count * blocksize * 100 ) / totalsize )
+                                strProgressBar = str( percent )
+                                #print percent  #DEBUG
+                                DIALOG_PROGRESS.update( percent , "downloading: %s " %  logo_list[select] , "path: %s" % full_logo_path )
+                            if os.path.exists(logo_path.encode("utf-8")):
+                                fp , h = urllib.urlretrieve( url_logo , full_logo_path , _report_hook )
+                                #print h
+                                DIALOG_PROGRESS.close
+                                print "### Logo downloaded Successfully !!!"
+                                xbmcgui.Dialog().ok("Success" , "Logo downloaded successfully !" )
+                            else: 
+                                print "### Logo download Failed !!!"
+                                print_exc()
+                                xbmcgui.Dialog().ok("Little Error" , "Error detecting path !" )
+                                
+                        except :
+                            print_exc()
+                            DIALOG_PROGRESS.close   
+                            print "### Logo download error !!!"
+                            xbmcgui.Dialog().ok("Error" , "Error downloading logo !" )
+                    else: print "### Path not found"
+        else: xbmcgui.Dialog().ok("Error" , "Can't get logo from this view" )
         
     else:
         print "### Starting Bulk Mode"
@@ -219,7 +234,7 @@ if ( __name__ == "__main__" ):
                 tvcount = tvcount + 1
                 ratio =  int (float( tvcount  * 100 ) / total_tvshow )
                 print "### Checking %s TVshow: %s  id: %s" % ( ratio , translate_string( TVshow["name"] ) , TVshow["id"] )
-                if TVshow["id"][0:2] == "tt" : 
+                if TVshow["id"][0:2] == "tt" or TVshow["id"] == "": 
                     print "### IMDB id found in database(%s), checking for nfo" % TVshow["id"]
                     try: tvid = get_nfo_id( TVshow["path"] )
                     except:                
@@ -236,7 +251,9 @@ if ( __name__ == "__main__" ):
                     print "### Logo.png already exist, skiping ..."
                     total_logo = total_logo + 1
                     logo_url = False
-                else: logo_url = get_first_logo( tvid )
+                else: 
+                    print "### Search for a Logo..."
+                    logo_url = get_first_logo( tvid )
                 if logo_url: 
                     succes = download_logo( logo_url , TVshow["path"] , ratio , TVshow["name"] )
                     if succes: 
